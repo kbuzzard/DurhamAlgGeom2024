9292356da95e4f5d9f8430df735a46d5312a9de9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-11-20 12:18:11 +0000

    finished separated!

--- DurhamAlgGeom2024/Tutorial/06Separated.lean
+++ DurhamAlgGeom2024/Tutorial/06Separated.lean
@@ -28,14 +28,11 @@ but there is an injective map between the two rings so it's OK.
 2) [TODO] From (1) we can deduce that there's a bilinear map `S_{(f)} × S_{(g)} → S_{(fg)}`
 and hence a map `S_{(f)} ⊗[ℤ] S_{(g)} → S_{(fg)}`. We prove that this map
 is surjective.
-
 -/
 
 section ring_theory_lifting
-
 -- let A,B,C be commutative rings
 variable {A B C : Type} [CommRing A] [CommRing B] [CommRing C]
-
 -- Say we have ring homs `φ : A → C` and `ψ : B → C`, with the following two
 -- properties:
 -- (i) im(φ) ⊆ im(ψ)
@@ -52,7 +49,6 @@ a function, not a ring homomorphism. See `lift_of_range_sub_range_of_injective`
 for the ring homomorphism. -/
 private noncomputable def lift_aux : A → B :=
   fun a ↦ (hi ⟨a, rfl⟩).choose -- uses the axiom of choice in Lean's type theory
-
 -- The fact that `lift_aux` is a lift
 @[simp]
 lemma lift_aux_spec (a : A) : ψ (lift_aux hi a) = φ a :=
@@ -64,18 +60,21 @@ the assumption that im(φ) ⊆ im(ψ). -/
 noncomputable def lift_of_range_sub_range_of_injective : A →+* B where
   toFun := lift_aux hi
   map_one' := by
-    sorry
+    apply hii
+    simp
   map_mul' x y := by
-    sorry
+    apply hii
+    simp only [lift_aux_spec, map_mul]
   map_zero' := by
-    sorry
+    apply hii
+    simp
   map_add' x y := by
-    sorry
+    apply hii
+    simp
 
 end ring_theory_lifting
 
 noncomputable section part1
-
 -- missing API for `Localization.awayLift`
 lemma Localization.awayLift_mk {R S} [CommRing R] [CommRing S]
     (f : R →+* S) (r : R) (a : R) (v : S) (hv : f r * v = 1) (j : ℕ) :
@@ -99,11 +98,9 @@ variable {d e : ℕ}
 -- Say f and g are homogeneous of degrees d and e
 variable {f : S} (hf : f ∈ 𝒜 d)
 variable {g : S} (hg : g ∈ 𝒜 e)
-
 -- let's not have to type HomogeneousLocalization everywhere
 open HomogeneousLocalization
 open Submonoid (powers)
-
 -- Let's define a map from S_(f) to S_(fg)
 variable {x : S} (hx : x = f * g)
 
@@ -125,15 +122,11 @@ S_{(f)} -----------------> S_{(fg)}
  |                             |
  \/    universal property      \/
 S[1/f] -----------------> S[1/(fg)]
-
-
 -/
-
 -- map1 is the diagonal map; we will later on lift it to get what we want
 def map1 : Away 𝒜 f →+* Localization.Away x :=
   (Localization.awayLift (algebraMap S _) _ (lemma1 hx)).comp
     (algebraMap (Away 𝒜 f) (Localization.Away f))
-
 -- this could be golfed/tidied
 include hg
 lemma lemma2 : Set.range (map1 𝒜 (f := f) hx) ⊆ Set.range (val (𝒜 := 𝒜)) := by
@@ -164,8 +157,7 @@ lemma lemma2 : Set.range (map1 𝒜 (f := f) hx) ⊆ Set.range (val (𝒜 := �
 def map2 : Away 𝒜 f →+* Away 𝒜 x :=
   lift_of_range_sub_range_of_injective (φ := map1 𝒜 hx)
   (ψ := algebraMap (Away 𝒜 x) (Localization.Away x))
-  (lemma2 𝒜 hg hx)
-
+  (lemma2 𝒜 hg hx) (val_injective _)
 -- the defining property of map2
 lemma map2_spec :
     (algebraMap (Away 𝒜 x) (Localization.Away x)).comp (map2 𝒜 hg hx) = map1 𝒜 hx := by
@@ -174,11 +166,16 @@ lemma map2_spec :
   have foo : Set.range ⇑(map1 𝒜 hx) ⊆ Set.range ⇑(algebraMap (Away 𝒜 x) (Localization.Away x)) :=
     lemma2 𝒜 hg hx
   exact lift_aux_spec foo a
+@[simp]
+lemma map2_spec' (z) :
+    (map2 𝒜 hg hx z).val = map1 𝒜 hx z := by
+  have foo : Set.range ⇑(map1 𝒜 hx) ⊆ Set.range ⇑(algebraMap (Away 𝒜 x) (Localization.Away x)) :=
+    lemma2 𝒜 hg hx
+  exact lift_aux_spec foo z
 
 /-
 
 ## We now prove part (2)
-
 -/
 
 omit hx -- no longer needed
@@ -190,7 +187,38 @@ def tensormap : Away 𝒜 f ⊗[ℤ] Away 𝒜 g →+* Away 𝒜 (f * g) :=
     (RingHom.toIntAlgHom <| map2 𝒜 hg rfl)
     (RingHom.toIntAlgHom <| map2 𝒜 hf <| mul_comm f g)
     (by intros; apply Commute.all)).toRingHom
-
 -- part (2)
-lemma tensormap_surjective : Function.Surjective (tensormap 𝒜 hf hg) := by
-  sorry
+lemma tensormap_surjective (hd:d ≠0) : Function.Surjective (tensormap 𝒜 hf hg) := by
+  unfold Function.Surjective
+  intro z
+  obtain ⟨⟨n, ⟨a, ha⟩, ⟨b, hb'⟩, ⟨j, (rfl : _ = b)⟩⟩, rfl⟩ := mk_surjective z
+  dsimp at hb'
+  dsimp
+  let x0 : NumDenSameDeg 𝒜 (.powers f) := {
+    deg := j*(d*(e+1))
+    num := ⟨a*g^(j*(d-1)),sorry⟩
+    den := ⟨f^(j*(e+1)),sorry⟩
+    den_mem := ⟨_,rfl⟩
+  }
+  let y0 : NumDenSameDeg 𝒜 (.powers g) := {
+    deg := j*(d*e)
+    num := ⟨f^(j*e),sorry⟩
+    den := ⟨g^(j*d),sorry⟩
+    den_mem := ⟨_,rfl⟩
+  }
+  use (mk x0 ⊗ₜ[ℤ] mk y0)
+  simp [tensormap]
+  ext
+  simp [map1]
+  rw [Localization.awayLift_mk (hv:=lemma1' rfl)]
+  rw [Localization.awayLift_mk (hv:=lemma1' (mul_comm _ _))]
+  simp [Localization.mk_mul, ← Localization.mk_one_eq_algebraMap, Localization.mk_pow]
+  rw [Localization.mk_eq_mk_iff]
+  rw [Localization.r_iff_exists]
+  dsimp
+  use 1
+  simp
+  cases d
+  contradiction
+  simp
+  ring
